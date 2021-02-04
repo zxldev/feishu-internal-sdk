89ab616afaa3a1ebb199eb26b7777e894ab34c8a
zxldev <zxldev@.live.com>
2021-02-04 12:15:18 +0800

    拆分card

--- sdk/message.go
+++ sdk/message.go
@@ -19,7 +19,40 @@ func (t Tenant) SendMessage(msg model.Msg) {
 	}
 }
 
-func (t Tenant) RobotMessageCallback(w http.ResponseWriter, r *http.Request, eventCallback func(event model.Event), action func(button model.EventAction)) (err error) {
+func (t Tenant) MessageCardCallback(w http.ResponseWriter, r *http.Request, action func(button model.EventAction)) (err error) {
+	event := model.Event{}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &event)
+	if err != nil {
+		return
+	}
+	if event.Token != FeiShu.VerificationToken {
+		w.WriteHeader(404)
+		return
+	}
+	if event.Type != "" {
+		if event.Type == "url_verification" {
+			w.Write([]byte(`{"challenge":"` + event.Challenge + `"}`))
+			return
+		}
+	}
+	//开始按照button 回调处理
+
+	actionData := model.EventAction{}
+	err = json.Unmarshal(body, &actionData)
+	if err != nil {
+		return
+	}
+	action(actionData)
+	return
+}
+
+func (t Tenant) EventCallback(w http.ResponseWriter, r *http.Request, eventCallback func(event model.Event)) (err error) {
 	event := model.Event{}
 
 	body, err := ioutil.ReadAll(r.Body)
@@ -58,14 +91,5 @@ func (t Tenant) RobotMessageCallback(w http.ResponseWriter, r *http.Request, eve
 		}
 	}
 	//TODO v2 的消息 https://open.feishu.cn/document/ukTMukTMukTM/uUTNz4SN1MjL1UzM
-
-	//开始按照button 回调处理
-
-	actionData := model.EventAction{}
-	err = json.Unmarshal(body, &actionData)
-	if err != nil {
-		return
-	}
-	action(actionData)
 	return
 }
